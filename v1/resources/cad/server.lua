68b5d145f7c4eb906e46f5d2979e58f91ff37aea
ScottUK <scottukgaming@gmail.com>
2018-11-17 06:31:32 +0000

    Update server.lua

--- v1/resources/cad/server.lua
+++ v1/resources/cad/server.lua
@@ -1,7 +1,3 @@
-local RandomEndpoints = {
-	['identities'] = { Message = 'people are currently signed up to our CAD!' }
-}
-
 AddEventHandler('playerConnecting', function()
     if not string.find(GetPlayerIdentifiers(source)[1], "steam:") then
 		deferrals.defer()
@@ -10,21 +6,21 @@ AddEventHandler('playerConnecting', function()
 end)
 
 Citizen.CreateThread(function()
-    while true do
-		for a = 1, #RandomEndpoints do
-			PerformHttpRequest(Config['Website'] .. '/api?endpoint=' .. RandomEndpoints[a], function(statusCode, response, headers)
-				if response then
-					local Information = json.decode(response)
-					local Amount = 0
-					for b = 1, #Information
-						Amount = Amount + 1
-					end
-					TriggerClientEvent('chatMessage', -1, '', {255, 255, 255}, Amount .. ' ' .. Message)
-				end      
-			end)
-			Citizen.Wait(300000)
-		end
-		Citizen.Wait(300000)
+    if Config['AutomaticMessages'] then
+        while true do
+            PerformHttpRequest(Config['Website'] .. '/api.php?endpoint=identities', function(statusCode, response, headers)
+                if response then
+                    local Information = json.decode(response)
+                    local Amount = 0
+                    Information = Information.content
+                    for a = 1, #Information do
+                        Amount = Amount + 1 
+                    end
+                    TriggerClientEvent('chatMessage', -1, '', {255, 255, 255}, Config['ChatPrefix'] .. '^7 We currently have ^4' .. Amount .. '^7 characters registered in our CAD system! ^2' .. Config['Website'])
+                end
+            end)
+    		Citizen.Wait(Config['AutomaticMessagesTime'] * 60000)
+        end
     end
 end)
 
