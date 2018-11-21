924ddc69865410354ae7b131040bd6fc00dd897a
Avery Johnson <avery627@gmail.com>
2018-11-21 15:28:47 -0500

    Error Handling

--- v1/upload/includes/connect.php
+++ v1/upload/includes/connect.php
@@ -2,20 +2,25 @@
 
 // MySQL Settings
 define("DB_HOST", "localhost");
-define("DB_USER", "root");
+define("DB_USER", "root2");
 define("DB_PASS", "");
 define("DB_NAME", "hydrid_v1");
 
+require('functions.php');
 
 // Do Not Edit Below --- SERIOUSLY DON'T TOUCH THIS STUFF.
 $pdoOptions = array(
     PDO::ATTR_ERRMODE => PDO::ERRMODE_SILENT,
     PDO::ATTR_EMULATE_PREPARES => false
 );
-
-$pdo = new PDO(
-    "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME,
-    DB_USER,
-    DB_PASS,
-    $pdoOptions
-);
+try {
+    $pdo = new PDO(
+        "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME,
+        DB_USER,
+        DB_PASS,
+        $pdoOptions
+    );
+} catch (Exception $e) {
+    throwError('Unable to connect to database.', true);
+    die('Unable to connect to database.');
+}
