73e26230c9b4f76f01df9053af62df6df5c62fd8
Tyler-HS <45082353+Tyler-HS@users.noreply.github.com>
2019-01-20 09:35:03 -0500

    ^

--- v1/sql/updates/update [v1.0.6]/UPDATE_v1.0.6.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE `departments`;
-ALTER TABLE `settings` DROP `site_url`;
-ALTER TABLE `settings` DROP `button_theme`;
-ALTER TABLE `settings` DROP `panel_suspended`;
-ALTER TABLE `users` DROP `departments`;
-ALTER TABLE `users` ADD `first_login` INT(11) NOT NULL DEFAULT '0' COMMENT '0/Yes - 1/No' AFTER `discord`;
-ALTER TABLE `users` DROP `email`;
--- v1/upload/functions/user/auth/userLogin.php
+++
@@ -1,8 +0,0 @@
-<?php
-require('../../../includes/connect.php');
-include('../../../includes/config.php');
-session_start();
-
-require('../../../classes/lib/password.php');
-
-userLogin($_POST['username'], $_POST['password']);
