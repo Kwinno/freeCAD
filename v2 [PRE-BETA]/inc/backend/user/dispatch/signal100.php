c3e80fe2d6b38c684484088e7832bb678933daa1
Tyler-HS <45082353+Tyler-HS@users.noreply.github.com>
2019-01-23 12:38:35 -0500

    alert fix again bc im stuoid

--- v2 [PRE-BETA]/inc/backend/user/dispatch/signal100.php
+++ v2 [PRE-BETA]/inc/backend/user/dispatch/signal100.php
@@ -16,17 +16,20 @@ if (isset($_SESSION['on_duty']) && $_SESSION['on_duty'] === "Dispatch") {
     $pb_row = $stmt->fetch(PDO::FETCH_ASSOC);
 
     if ($pb_row['priority'] === 1) {
-
+        if ($settings['discord_alerts'] === 'true') {
         discordAlert('**Signal 100 Is No Longer In Effect**
             Dispatcher ('.$_SESSION['identity_name'].') Has Deactivated Signal 100
             - **Hydrid CAD System**');
+        }
         $stmt4              = $pdo->prepare("UPDATE `servers` SET `priority`='0' WHERE `id`=:server_id");
         $stmt4->bindValue(':server_id', $_SESSION['server']);
         $result = $stmt4->execute();
     } else {
+        if ($settings['discord_alerts'] === 'true') {
         discordAlert('**Signal 100 IS NOW IN EFFECT**
             Dispatcher ('.$_SESSION['identity_name'].') Has Activated Signal 100 - Check CAD For Further Details
             - **Hydrid CAD System**');
+        }
         $stmt3              = $pdo->prepare("UPDATE `servers` SET `priority`='1' WHERE `id`=:server_id");
         $stmt3->bindValue(':server_id', $_SESSION['server']);
         $result = $stmt3->execute();
