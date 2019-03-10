fbe0eb7acc14476c84e564245fb1b121279d156c
Tyler-HS <45082353+Tyler-HS@users.noreply.github.com>
2019-03-10 13:12:08 -0400

    copyright update

--- v2 [PRE-BETA]/inc/copyright.php
+++ v2 [PRE-BETA]/inc/copyright.php
@@ -2,7 +2,7 @@
             <div class="container">
                 <div class="row">
                     <div class="col-12 text-center">
-                    2017 - <?php echo date('Y'); ?> | Developed by <strong><a href="https://discord.gg/NeRrWZC">Tyler#7918</a></strong> <br>
+                    2017 - <?php echo date('Y'); ?> | Powered by <strong><a href="https://discord.gg/NeRrWZC">freeCAD</a></strong> <br>
                     Version: <?php echo $version; ?>
                     </div>
                 </div>
